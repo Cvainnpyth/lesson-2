d98fcd67a2a95b3ab52b10f7864cc60e6292b041
shcheglov812@gmail.com <JuhU3Zm@VVN9Mvm>
2023-03-10 01:33:30 +0300

    homework

---
+++ variables.py
@@ -0,0 +1,19 @@
+# Объект для описание - сотрудник (employee)
+# Объявление переменных основных типов данных
+"""name = str ( input ('Введите Ваше имя'))
+age = int (input ('Введите Ваш возраст'))
+pay_rate = float (input ('Введите почасовую ставку заработной платы'))
+hours = str ( input ('Введите количество отработанных часов'))"""
+
+name = 'Sergei'
+age = 36
+pay_rate = 350.5
+is_married = True
+
+print(type(name))
+print(type(age))
+print(type(pay_rate))
+print(type(is_married))
+
+
+
